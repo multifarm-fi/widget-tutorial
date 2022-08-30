d36bfd68994c006ef2e2b4d52e4ee1d01ab0847f
tg <tadasgedgaudas@icloud.com>
2022-08-30 16:39:59 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import { MultifarmProvider, Dashboard } from '@multifarm/widget'
+
+export default function App() {
+  return (
+    <MultifarmProvider token="" theme="maia">
+      <Dashboard/>
+    </MultifarmProvider>
+  )
+}
