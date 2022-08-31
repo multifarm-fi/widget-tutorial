b71733e161b95b41fd208f52debcb1c4fc0f8d92
S. Sarantsev <sarantsev.s@gmail.com>
2022-08-31 21:57:47 +0200

    fix styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import { MultifarmProvider, Dashboard } from '@multifarm/widget'
+import '@multifarm/widget/dist/maia.css'
 
 export default function App() {
 
   return (
-      <MultifarmProvider token={process.env.REACT_APP_TOKEN} theme="maia" key="maia" provider="maia" 
-      themeColors='linear-gradient(197.3deg, #072154 0%, #345FB0 22.92%, #9B74D1 48.96%, #B67ADA 64.69%, #8A62BC 82.29%, #453B8D 100%)'>
+      <MultifarmProvider
+          token={process.env.REACT_APP_TOKEN}
+          theme="maia"
+          key="maia"
+          provider="maia"
+      >
         <Dashboard />
       </MultifarmProvider>
     )
