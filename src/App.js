a8e83f0d07a2b638a3398ab42cb0897dd5c59e6d
S. Sarantsev <sarantsev.s@gmail.com>
2022-08-31 23:02:06 +0200

    fix colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import { MultifarmProvider, Dashboard } from '@multifarm/widget'
+import '@multifarm/widget/dist/maia.css'
+
+const GRADIENT = 'linear-gradient(197.3deg, #072154 0%, #345FB0 22.92%, #9B74D1 48.96%, #B67ADA 64.69%, #8A62BC 82.29%, #453B8D 100%)'
 
 export default function App() {
 
   return (
-      <MultifarmProvider token={process.env.REACT_APP_TOKEN} theme="maia" key="maia" provider="maia" 
-      themeColors='linear-gradient(197.3deg, #072154 0%, #345FB0 22.92%, #9B74D1 48.96%, #B67ADA 64.69%, #8A62BC 82.29%, #453B8D 100%)'>
-        <Dashboard />
-      </MultifarmProvider>
+      <div style={{backgroundImage: GRADIENT, padding: '100px'}}>
+          <MultifarmProvider
+              token={process.env.REACT_APP_TOKEN}
+              theme="maia"
+              key="maia"
+              provider="maia"
+          >
+              <Dashboard />
+          </MultifarmProvider>
+      </div>
     )
 }
