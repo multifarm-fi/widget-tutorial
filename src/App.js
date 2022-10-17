6b5d85f148e639a71c2253e2adeb4fcfe34e0dcd
Tadas Gedgaudas <tadasgedgaudas@icloud.com>
2022-10-17 16:11:47 +0300

    Add redacted example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import { MultifarmProvider, Dashboard } from '@multifarm/widget'
-import '@multifarm/widget/dist/maia.css'
+import '@multifarm/widget/dist/redacted.css'
 
-const GRADIENT = 'linear-gradient(197.3deg, #072154 0%, #345FB0 22.92%, #9B74D1 48.96%, #B67ADA 64.69%, #8A62BC 82.29%, #453B8D 100%)'
+const GRADIENT = '#040309'
 
 export default function App() {
 
@@ -10,10 +10,24 @@ export default function App() {
       <div style={{backgroundImage: GRADIENT, padding: '100px'}}>
           <MultifarmProvider
               token={process.env.REACT_APP_TOKEN}
-              theme="maia"
-              key="maia"
-              provider="maia"
+              theme="redacted"
+              key="redacted"
+              provider="redacted"
               themeColors="dark"
+              strategyTypesConfig={{
+                wallet: {
+                  cols: {
+                    apr_y: false,
+                    month_earnings: false
+                  }
+                },
+                claimable: {
+                  cols: {
+                    apr_y: false,
+                    month_earnings: false
+                  }
+                }
+              }}
           >
               <Dashboard />
           </MultifarmProvider>
